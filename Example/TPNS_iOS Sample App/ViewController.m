16fc5ac86540a2bacd56c19620835eb44ad2916d
Carl Jahn <jahncarl@gmail.com>
2016-02-25 11:58:09 +0100

    Example App: removed not needed methods

--- Example/TPNS_iOS Sample App/ViewController.m
+++ Example/TPNS_iOS Sample App/ViewController.m
@@ -15,15 +15,6 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 - (IBAction)unregisterAction:(id)sender
 {
