fcb1bfd1ad8dab995a5fb4f84355108e9b105848
Carl Jahn <jahncarl@gmail.com>
2016-02-25 10:30:01 +0100

    fixed Header Info

--- TPNS_iOS/DTPushNotification.m
+++ TPNS_iOS/DTPushNotification.m
@@ -2,7 +2,7 @@
 //  DTPushNotification.m
 //  DTPushNotification
 //
-//  Created by Björn Richter on 12.02.16.
+//  Created by Deutsche Telekom AG on 12.02.16.
 //  Copyright © 2016 Deutsche Telekom AG. All rights reserved.
 //
 
