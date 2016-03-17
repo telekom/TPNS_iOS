27b4678ff49e9a144628214472186855c78dfde4
Carl Jahn <jahncarl@gmail.com>
2016-03-17 11:27:47 +0100

    test

---
+++ TPNS/TPNS.h
@@ -0,0 +1,19 @@
+//
+//  TPNS.h
+//  TPNS
+//
+//  Created by Carl Jahn on 17.03.16.
+//  Copyright © 2016 Deutsche Telekom AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPNS.
+FOUNDATION_EXPORT double TPNSVersionNumber;
+
+//! Project version string for TPNS.
+FOUNDATION_EXPORT const unsigned char TPNSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPNS/PublicHeader.h>
+
+
