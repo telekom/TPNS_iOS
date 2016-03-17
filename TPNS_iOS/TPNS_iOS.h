fe00caa9516d1ee3177fd29233b9579254a13771
Carl Jahn <jahncarl@gmail.com>
2016-03-17 11:34:59 +0100

    fixed umbrella header warning

---
+++ TPNS_iOS/TPNS_iOS.h
@@ -0,0 +1,12 @@
+//
+//  TPNS.h
+//  TPNS
+//
+//  Created by Carl Jahn on 17.03.16.
+//  Copyright © 2016 Deutsche Telekom AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTPushNotification.h"
+
